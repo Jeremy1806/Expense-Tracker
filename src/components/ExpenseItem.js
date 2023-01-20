181cd7bd981eaaa2c4fdaa9981b1a7f3ba977359
Jeremy_1806 <priyanshu.singh@capriglobal.in>
2023-01-20 15:50:55 +0530

    building first component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>Lets Develop This, Learning React</div>
+      <div>
+        <h2>First Expense</h2>
+        <div>Rs.110</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
