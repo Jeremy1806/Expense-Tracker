82c9a8cb8196fa0e3be25a708a64d6057578c921
Jeremy_1806 <priyanshu.singh@capriglobal.in>
2023-01-25 12:12:34 +0530

    added Card, ExpenseDate, and Expenses component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   return (
     <div>
-      <ExpenseItem />
+      <Expenses />
     </div>
   );
 }
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,21 @@
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  const expenseDate = props.date;
+  const expenseTitle = props.title;
+  const expensePrice = props.amount;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={expenseDate} />
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">Rs.{expensePrice}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
